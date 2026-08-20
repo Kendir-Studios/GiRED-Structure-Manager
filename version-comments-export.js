b926401d444f1ccc293c47eb714896d7053575d5
Kendir Studios <programacao@kendirstudios.pt>
2026-08-20 11:17:22 +0100

    Update export branding to GiRED Fixer

--- version-comments-export.js
+++ version-comments-export.js
@@ -511,7 +511,7 @@
         button.id = EXPORT_BUTTON_ID;
         button.type = "button";
         button.className = nativeButton.className;
-        button.title = "Carregar todos os comentários e exportar com o GiRED Structure Mapper";
+        button.title = "Carregar todos os comentários e exportar com o GiRED Fixer";
         button.setAttribute("aria-label", button.title);
         button.innerHTML = '<svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg> Excel Mapper';
         button.addEventListener("click", exportAllComments);
